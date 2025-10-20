d16ed00635635f3a97fb2945240c1b9f67531b6f
ban <jmburke4@crimson.ua.edu>
2025-10-20 10:17:36 -0500

    Add batch file, update Program.cs
    Add Lox.bat to serve as shortcut for running the executable from the root directory of the project, accepts file path argument.
    
    Update Program.cs to add Run, RunFile, and RunPrompt methods to wrap the basic input/output.

---
+++ Lox.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Can add /B to make the program run in the same window, but it gets confusing 
+rem about which process (lox or the shell) is recieving input
+start "" ".\bin\Debug\net9.0\LoxInterpreter.exe" %*
--- Program.cs
+++ Program.cs
@@ -2,11 +2,55 @@
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello there! General Kenobi...");
+        if (args.Length > 1)
+        {
+            Console.WriteLine("Usage: loxinterpreter <script> | lox <script>");
+            return;
+        }
+        else if (args.Length == 1)
+        {
+            RunFile(args[0]);
+        }
+        else
+        {
+            RunPrompt();
+        }
+    }
 
-        // Readkey to close external console window when using debugger
-        Console.ReadKey();
+    private static void Run(string line)
+    {
+        Console.WriteLine(line);
+    }
+
+    private static void RunFile(string path)
+    {
+        try
+        {
+            var lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                Run(line);
+            }
+            RunPrompt();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in runFile: {ex.Message}");
+        }
+    }
+
+    private static void RunPrompt()
+    {
+        Console.WriteLine("Welcome to the Lox Intepreter REPL! \nType exit or ctrl + c to exit...");
+        string? line;
+        while (true)
+        {
+            Console.Write("\n>");
+            line = Console.ReadLine();
+            if (line == null || line == "exit") break;
+            Run(line);
+        }
     }
 }
