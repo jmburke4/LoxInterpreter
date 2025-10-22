3e6027c0f2a5637df46181f6db0915dceb2b4890
ban <jmburke4@crimson.ua.edu>
2025-10-22 16:23:04 -0500

    Refactor GenerateAst to use primary constructors with private fields backing public getters

--- Ast.bat
+++ Ast.bat
@@ -1,6 +1,3 @@
 @echo off
-rem Can add /B to make the program run in the same window, but it gets confusing 
-rem about which process (lox or the shell) is recieving input
-
-rem Or I can just shorten the dotnet run command, but I like the new window
-start "" ".\GenerateAst\bin\Debug\net9.0\GenerateAst.exe" "R:\\LoxInterpreter\\LoxInterpreter"
+    
+dotnet run --project ./GenerateAst/GenerateAst.csproj "R:\\LoxInterpreter\\LoxInterpreter"
