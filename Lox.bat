8f65dff23195b0b404e4802e8c6c59f22bd492d9
ban <jmburke4@crimson.ua.edu>
2025-10-22 08:25:25 -0500

    Update directory structure to put project in its own folder

--- Lox.bat
+++ Lox.bat
@@ -1,4 +1,6 @@
 @echo off
 rem Can add /B to make the program run in the same window, but it gets confusing 
 rem about which process (lox or the shell) is recieving input
-start "" ".\bin\Debug\net9.0\LoxInterpreter.exe" %*
+
+rem Or I can just shorten the dotnet run command, but I like the new window
+start "" ".\LoxInterpreter\bin\Debug\net9.0\LoxInterpreter.exe" %*
