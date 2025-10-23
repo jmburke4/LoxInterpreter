052e55460eea3d659a27f8b5636c2edf053050d5
Jackson Burke <jackson.burke@nucor.com>
2025-10-23 10:42:48 -0500

    Add VisitorPatternExample project

---
+++ VisitorPatternExample/ComputerPart.cs
@@ -0,0 +1,46 @@
+﻿namespace VisitorPatternExample;
+
+// Element interface
+public interface IComputerPart
+{
+    void Accept(IComputerPartVisitor visitor);
+}
+
+// Concrete Elements
+public class Keyboard : IComputerPart
+{
+    public void Accept(IComputerPartVisitor visitor)
+    {
+        visitor.Visit(this); // Calls the Visitor's visit method for Keyboard
+    }
+}
+
+public class Monitor : IComputerPart
+{
+    public void Accept(IComputerPartVisitor visitor)
+    {
+        visitor.Visit(this); // Calls the Visitor's visit method for Monitor
+    }
+}
+
+// Visitor interface
+public interface IComputerPartVisitor
+{
+    void Visit(Keyboard keyboard);
+    void Visit(Monitor monitor);
+}
+
+// Concrete Visitor
+public class ComputerPartDisplayVisitor : IComputerPartVisitor
+{
+    public void Visit(Keyboard keyboard)
+    {
+        Console.WriteLine("Displaying Keyboard.");
+    }
+
+    public void Visit(Monitor monitor)
+    {
+        Console.WriteLine("Displaying Monitor.");
+    }
+}
+
---
+++ VisitorPatternExample/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace VisitorPatternExample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var computerParts = new List<IComputerPart>
+        {
+            new Keyboard(),
+            new Monitor()
+        };
+
+        var visitor = new ComputerPartDisplayVisitor();
+
+        foreach (var part in computerParts)
+        {
+            part.Accept(visitor); // Part calls the correct visitor method
+        }
+
+        Console.ReadKey();
+    }
+}
