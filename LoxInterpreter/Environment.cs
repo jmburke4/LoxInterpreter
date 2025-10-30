1494b9f7658aad28c84b3e521b683463c01ec629
ban <jmburke4@crimson.ua.edu>
2025-10-30 16:21:20 -0400

    Add scopes to Environment

--- LoxInterpreter/Environment.cs
+++ LoxInterpreter/Environment.cs
@@ -1,13 +1,16 @@
 namespace LoxInterpreter;
 
-public class Environment
+public class Environment(Environment? enclosing = null)
 {
     // See also OrderedDictionary which maintains keys ordered by the sequence they are added in
     private readonly Dictionary<string, object?> Values = [];
 
+    private readonly Environment? Enclosing = enclosing;
+
     public void Assign(Token name, object? value)
     {
         if (Values.ContainsKey(name.Lexeme)) Values[name.Lexeme] = value;
+        else if (Enclosing != null) Enclosing.Assign(name, value);
         else throw new RuntimeError(name, $"Undefined variable '{name.Lexeme}'.");
     }
 
@@ -16,6 +19,7 @@ public class Environment
     public object? Get(Token name)
     {
         if (Values.ContainsKey(name.Lexeme)) return Values.GetValueOrDefault(name.Lexeme);
+        if (Enclosing != null) return Enclosing.Get(name);
         throw new RuntimeError(name, $"Undefined variable '{name.Lexeme}'.");
     }
 }
