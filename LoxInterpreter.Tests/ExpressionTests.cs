7653ce5447e30b4d1bf2077d1eb61aa589893284
ban <jmburke4@crimson.ua.edu>
2025-11-07 10:59:09 -0600

    Reorganize tests to use theories with inline data instead of facts (#25)

--- LoxInterpreter.Tests/ExpressionTests.cs
+++ LoxInterpreter.Tests/ExpressionTests.cs
@@ -5,7 +5,35 @@ namespace LoxInterpreter.Tests;
 /// </summary>
 public class ExpressionTests
 {
-    private static object? TestHelper(string input)
+    [Theory]
+    /// Order of Operations
+    [InlineData(14, "2 + 3 * 4;")]
+    [InlineData(8, "20 - 3 * 4;")]
+    [InlineData(4, "2 + 6 / 3;")]
+    [InlineData(0, "2 - 6 / 3;")]
+    [InlineData(4, "(2 * (6 - (2 + 2)));")]
+    /// Comparison precedence
+    [InlineData(true, "false == 2 < 1;")]
+    [InlineData(true, "false == 1 > 2;")]
+    [InlineData(true, "false == 2 <= 1;")]
+    [InlineData(true, "false == 1 >= 2;")]
+    [InlineData(false, "true == 2 < 1;")]
+    [InlineData(false, "true == 1 > 2;")]
+    [InlineData(false, "true == 2 <= 1;")]
+    [InlineData(false, "true == 1 >= 2;")]
+    /// Testing the spacing of minus
+    [InlineData(0, "1 - 1;")]
+    [InlineData(0, "1 -1;")]
+    [InlineData(0, "1- 1;")]
+    [InlineData(0, "1-1;")]
+    [InlineData(2, "1 - -1;")]
+    [InlineData(0, "1 + -1;")]
+    [InlineData(-1, "1 * -1;")]
+    [InlineData(-1, "1 / -1;")]
+    /// Divide by zero
+    [InlineData("nil", "1 / 0;")]
+    [InlineData("nil", "10 / 0 * 2;")]
+    public void Test(object? expected, string input)
     {
         ErrorHandler handler = new();
 
@@ -16,94 +44,9 @@ public class ExpressionTests
         var expr = (Stmt.Expression?)parser.Parse().FirstOrDefault();
 
         // Scanning or parsing error
-        if (handler.HadError || expr is null) return null;
+        if (handler.HadError || expr is null) Assert.Fail();
 
         Interpreter interpreter = new(handler);
-        return interpreter.Interpret(expr.Expr);
+        Assert.Equal(expected?.ToString(), interpreter.Interpret(expr.Expr).ToString());
     }
-
-    /// <summary>
-    /// Basic order of operations
-    /// </summary>
-
-    [Fact]
-    public void PlusStar() => Assert.Equal(14, (double?)TestHelper("2 + 3 * 4;"));
-
-    [Fact]
-    public void MinusStar() => Assert.Equal(8, (double?)TestHelper("20 - 3 * 4;"));
-
-    [Fact]
-    public void PlusSlash() => Assert.Equal(4, (double?)TestHelper("2 + 6 / 3;"));
-
-    [Fact]
-    public void MinusSlash() => Assert.Equal(0, (double?)TestHelper("2 - 6 / 3;"));
-
-    [Fact]
-    public void Grouping() => Assert.Equal(4, (double?)TestHelper("(2 * (6 - (2 + 2)));"));
-
-    /// <summary>
-    /// Comparison precedence
-    /// </summary>
-
-    [Fact]
-    public void EqLt() => Assert.Equal(true, TestHelper("false == 2 < 1;"));
-
-    [Fact]
-    public void EqGt() => Assert.Equal(true, TestHelper("false == 1 > 2;"));
-
-    [Fact]
-    public void EqLte() => Assert.Equal(true, TestHelper("false == 2 <= 1;"));
-
-    [Fact]
-    public void EqGte() => Assert.Equal(true, TestHelper("false == 1 >= 2;"));
-
-    [Fact]
-    public void EqLtF() => Assert.Equal(false, TestHelper("true == 2 < 1;"));
-
-    [Fact]
-    public void EqGtF() => Assert.Equal(false, TestHelper("true == 1 > 2;"));
-
-    [Fact]
-    public void EqLteF() => Assert.Equal(false, TestHelper("true == 2 <= 1;"));
-
-    [Fact]
-    public void EqGteF() => Assert.Equal(false, TestHelper("true == 1 >= 2;"));
-
-    /// <summary>
-    /// Testing the spacing of minus
-    /// </summary>
-
-    [Fact]
-    public void OneMinusOne() => Assert.Equal(0, (double?)TestHelper("1 - 1;"));
-
-    [Fact]
-    public void OneNegOne() => Assert.Equal(0, (double?)TestHelper("1 -1;"));
-
-    [Fact]
-    public void OneMinusOneOther() => Assert.Equal(0, (double?)TestHelper("1- 1;"));
-
-    [Fact]
-    public void OneMinusOneNoSpace() => Assert.Equal(0, (double?)TestHelper("1-1;"));
-
-    [Fact]
-    public void OneMinusNegOne() => Assert.Equal(2, (double?)TestHelper("1 - -1;"));
-
-    [Fact]
-    public void OnePlusNegOne() => Assert.Equal(0, (double?)TestHelper("1 + -1;"));
-
-    [Fact]
-    public void OneStarNegOne() => Assert.Equal(-1, (double?)TestHelper("1 * -1;"));
-
-    [Fact]
-    public void OneSlashNegOne() => Assert.Equal(-1, (double?)TestHelper("1 / -1;"));
-
-    /// <summary>
-    /// Divide by zero
-    /// </summary>
-
-    [Fact]
-    public void DivideByZero() => Assert.Equal("nil", TestHelper("1 / 0;"));
-
-    [Fact]
-    public void NestedDivideByZero() => Assert.Equal("nil", TestHelper("10 / 0 * 2;"));
 }
